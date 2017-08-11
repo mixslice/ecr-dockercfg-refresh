0c6329ff210dc9520150c7a9dff87812083adafb
bensonz <mr.bz@hotmail.com>
2017-08-11 15:30:07 +0800

    Read namespace form env configmap

--- ecr-dockercfg-refresh.sh
+++ ecr-dockercfg-refresh.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
-
-K8S_NAMESPACE_FILE=/var/run/secrets/kubernetes.io/serviceaccount/namespace
-
 refresh_secret()
 {
-  if [ "x$NAMESPACE" == "x" ]
-  then
-    if [ -r $K8S_NAMESPACE_FILE ]
-    then
-      NAMESPACE=`cat $K8S_NAMESPACE_FILE`
-    else
-      NAMESPACE=default
-    fi
+  if [ "x$REFRESH_NAMESPACES" == "x"]; then
+    REFRESH_NAMESPACES=default
   fi
+  REFRESH_NAMESPACES=${REFRESH_NAMESPACES//,/$'\n'}
   if [ "x$AWS_ACCOUNT" == "x" ]; then
     AWS_ACCOUNT=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep accountId | awk -F\" '{print $4}'`
   fi
@@ -25,16 +17,18 @@ refresh_secret()
   TOKEN=`aws ecr --region=${AWS_REGION} get-authorization-token --output text --query "authorizationData[].authorizationToken" | base64 -d | cut -f2 -d:`
   DOCKER_CFG_SECRET=`printf '{"%s":{"username":"AWS","password":"%s"}}' "https://${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com.cn" "${TOKEN}" | base64 | tr -d '\n'`
 
-cat <<EOF | kubectl apply -f -
+  for NS in $REFRESH_NAMESPACES; do
+    cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Secret
 data:
   .dockercfg: ${DOCKER_CFG_SECRET}
 metadata:
   name: ${SECRET_NAME}
-  namespace: ${NAMESPACE}
+  namespace: ${NS}
 type: kubernetes.io/dockercfg
 EOF
+  done
 }
 
 if [ "x$REFRESH_INTERVAL" == "x" ]; then REFRESH_INTERVAL=21600; fi
